4c1e3774f8fcd2176e2d7ba67d44b487383b4066
Axel Eduardo Murillo <axele@axele-macbookpro.roam.corp.google.com>
2020-01-10 15:27:19 -0800

    Planning

--- PFPackage/PFBooks/PFRaces/PFRace.java
+++ PFPackage/PFBooks/PFRaces/PFRace.java
@@ -3,4 +3,31 @@ package PFPackage.PFBooks.PFRaces;
 public interface PFRace {
     // class name
     
+    // descriptions
+
+    //default names
+    
+    //randomStartingAge
+
+    //random height & weight
+
+    //==RACIAL TRAITS==
+        //standard racial traits!
+            //Ability Score Modifiers!
+            //Size
+            //Type (humanoid/subtype)
+            //Base Speed 
+                //defacto speed
+                //special rules
+            //Languages (LIST)
+        //Defensive racial traits
+        //Feat & Skill racial traits
+        //Senses Racial traits
+        //Offense Racial traits
+
+        //ALTERNATE RACIAL TRAITS
+            //may be selected in place of one(or more) std racial traits
+                
+
+
 }
